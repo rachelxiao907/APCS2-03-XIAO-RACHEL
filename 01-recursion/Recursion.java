3ba53a8310275757e5e02661cf2496a56ab5b99d
Rachel Xiao <rachelxiao907@gmail.com>
2021-02-06 12:35:27 -0500

    Added case for 0 in sqrt and changed guess to n/2

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -17,18 +17,21 @@ public class Recursion {
   */
   public static double sqrt(double n) {
     //Hint: This is a wrapper method
-    return sqrt(n, 1);
+    if (n == 0) {
+      return 0;
+    }
+    return sqrt(n, n / 2); //guess any number for the sqrt of n. (like n/2, or even 1)
   }
   /*
   *@param n : any non-negative value you want to take the sqrt of
   *@param guess : guess any number for the sqrt of n
-  *@return the approximate sqrt of n within a certain tolerance
+  *@return the approximate sqrt of n within a tolerance of 0.001%
   */
   public static double sqrt(double n, double guess) {
-    if (Math.abs((guess * guess - n) / n) * 100 < 0.001) {  //done until guess squared is close to n by a % difference of some threshold
+    if (Math.abs((guess * guess - n) / n) * 100 < 0.001) { //done until guess squared is close to n by a % difference of some threshold
       return guess;
     }
-    return sqrt(n, (n / guess + guess) / 2);
+    return sqrt(n, (n / guess + guess) / 2); //make a better guess
   }
 
   public static void main(String[] args) {
