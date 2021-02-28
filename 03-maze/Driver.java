3b74d064c019793105f21254a4f4cd5889771161
Rachel Xiao <rachelxiao907@gmail.com>
2021-02-28 17:44:40 -0500

    Added more mazes

--- 03-maze/Driver.java
+++ 03-maze/Driver.java
@@ -15,7 +15,6 @@ public class Driver{
         System.out.println("Invalid filename: " + filename);
       }
 
-
       String filename2 = "data2.dat";
       try {
         Maze f2;
@@ -29,7 +28,6 @@ public class Driver{
         System.out.println("Invalid filename: " + filename2);
       }
 
-
       String filename3 = "data3.dat";
       try {
         Maze f3;
@@ -42,5 +40,32 @@ public class Driver{
       catch(FileNotFoundException e) {
         System.out.println("Invalid filename: " + filename3);
       }
+
+      String filename4 = "Maze1.txt";
+      try {
+        Maze f4;
+        f4 = new Maze(filename4);//true animates the maze.
+
+        f4.setAnimate(true); //comment this if you are using windows cmd/powershell
+        System.out.println(f4.solve() + " steps");
+        System.out.println(f4);
+      }
+      catch(FileNotFoundException e) {
+        System.out.println("Invalid filename: " + filename4);
+      }
+
+      String filename5 = "LargeMazeWindows.txt";
+      try {
+        Maze f5;
+        f5 = new Maze(filename5);//true animates the maze.
+
+        f5.setAnimate(true); //comment this if you are using windows cmd/powershell
+        System.out.println(f5.solve() + " steps");
+        System.out.println(f5);
+      }
+      catch(FileNotFoundException e) {
+        System.out.println("Invalid filename: " + filename5);
+      }
+
     }
 }
