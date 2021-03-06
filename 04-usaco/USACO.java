fd98274e8d7ebf41a64cfa0b0aa0623b45d474a9
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-06 18:00:58 -0500

    Created stomp method to carry out stomp digging instructions

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -18,7 +18,27 @@ public class USACO {
         pasture[i][j] = row.nextInt();
       }
     }
+    stomp(pasture, 0, 0, 3);
     return pasture[0][0];
+  }
+
+  public static void stomp (int[][] pasture, int r, int c, int d) {
+    int max = pasture[r][c];
+    for (int i = r; i < r + 3; i++) { //loop through a 3x3 grid
+      for (int j = c; j < c + 3; j++) {
+        if (pasture[i][j] > max){
+          max = pasture[i][j]; //find the highest elevation in the 3x3 grid
+        }
+      }
+    }
+    max = max - d; //final elevation after being stomped
+    for (int i = r; i < r + 3; i++) {
+      for (int j = c; j < c + 3; j++) {
+        if (pasture[i][j] > max){
+          pasture[i][j] = max; //stomp the square
+        }
+      }
+    }
   }
 
   //public static long silver(String filename)
