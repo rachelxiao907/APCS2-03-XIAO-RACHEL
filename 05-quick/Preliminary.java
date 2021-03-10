320f42592aa1c69740c460dee9b3f586f645cc87
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-09 19:53:04 -0500

    I think I separated values by left and right side

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -15,25 +15,51 @@ public class Preliminary {
     data[index] = data[start]; //swap start value and pivot value
     data[start] = pivot;
     index = start;
+    start++; //increase start by 1 to check other values
+    //sorting array according to pivot
+    while (start != end) {
+      if (data[start] > pivot) { //if greater than pivot, move to the end (right side)
+        int temp = data[start];
+        data[start] = data[end]; //swap data[start] and data[end]
+        data[end] = temp;
+        end--; //shorten middle section
+      } else { //if less than pivot, leave value on left side
+        start++; //shorten middle section
+      }
+    }
 
     return index;
   }
 
   public static void main(String[] args) {
     int[] data1 = {999,999,999,4,3,2,1,0,999,999,999};
-    System.out.println(partition(data1, 0, data1.length-1));
+    //System.out.println(partition(data1, 0, data1.length-1));
 
     int start = 0;
-    int index = (int)(Math.random() * data1.length); //select random index for pivot value
-    int pivot = data1[index]; //the corresponding element of random is designated to the pivot element
-    data1[index] = data1[start]; //swap start value and pivot value
+    int end = data1.length - 1;
+    int index = (int)(Math.random() * data1.length);
+    System.out.println(index);
+    int pivot = data1[index];
+    data1[index] = data1[start];
     data1[start] = pivot;
+    index = start;
+    start++;
+    while (start != end) {
+      if (data1[start] > pivot) {
+        int temp = data1[start];
+        data1[start] = data1[end];
+        data1[end] = temp;
+        end--;
+      } else {
+        start++;
+      }
+    }
+    System.out.println(index);
     String output = "";
     for (int i = 0; i < data1.length; i++) {
       output += data1[i] + " ";
     }
     System.out.println(output + "");
-
   }
 
 }
