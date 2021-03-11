1977cee648b623a38b9b39004dbccc2265df0975
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-10 21:17:56 -0500

    Fixed errors in partition and modified it based on documentation

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -10,12 +10,19 @@ public class Preliminary {
   *@return the index of the final position of the pivot element.
   */
   public static int partition (int [] data, int start, int end) {
-    if (start == end) return start;
-    if (data.length <= 1) return start; //fixed out of bounds error
-    int index = (int)(Math.random() * (end - start + 1)) + start; //random index from start to end inclusive is chosen
-    int pivot = data[index]; //the corresponding element of random is designated to the pivot element
+    if (data.length <= 1 || start == end) return start; //fixed out of bounds error
+
+    int index = (start + end) / 2 + start; //middle index of subset
+    //when choosing a pivot, use the median value of the lo, hi, and middle elements
+    if (data[end] >= data[start] && data[start] >= data[index] || data[end] <= data[start] && data[start] <= data[index]) {
+      index = start; //if start value is the median, make index of pivot start
+    } else if (data[start] >= data[end] && data[end] >= data[index] || data[start] <= data[end] && data[end] <= data[index]) {
+      index = end; //if start value is the median, make index of pivot end
+    }
+
+    int pivot = data[index]; //the corresponding element of median index is designated to the pivot element
     //System.out.println(index); //debugging
-    //System.out.println(data[index]);
+    //System.out.println(pivot);
     data[index] = data[start]; //swap start value and pivot value
     data[start] = pivot;
     index = start;
@@ -24,7 +31,7 @@ public class Preliminary {
     //sorting array according to pivot
     while (start != end) {
       int r = (int)(Math.random() * 2); //assigns r = 0 to right and r = 1 to left
-      if (data[start] > pivot || data[start] == pivot && r == 0) { //if greater than pivot, move to the end (right side)
+      if (data[start] > pivot || (data[start] == pivot && r == 0)) { //if greater than pivot, move to the end (right side)
         int temp = data[start];
         data[start] = data[end]; //swap data[start] and data[end]
         data[end] = temp;
@@ -41,19 +48,10 @@ public class Preliminary {
       index = start; //pivot index is the middle index
     } else { //if value is greater than pivot
       data[index] = data[start-1]; //move this value to left of pivot by swapping
-      data[start - 1] = pivot; //place pivot to the left of value
+      data[start - 1] = pivot; //place pivot before start value
       index = start - 1;
     }
     return index;
   }
-
-  /*
-  public static void main(String[] args) {
-    int[] data1 = {999,999,999,4,3,2,1,0,999,999,999};
-    System.out.println(partition(data1, 3, 7));
-    int[] data2 = {1,15,27,32,8,5,17,3};
-    System.out.println(partition(data2, 0, data2.length-1));
-  }
-  */
 
 }
