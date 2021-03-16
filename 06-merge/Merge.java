3b66ea733e0125a62df40894756fc5fede85616e
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-16 11:48:48 -0400

    Documentation

---
+++ 06-merge/Merge.java
@@ -0,0 +1,22 @@
+public class Merge {
+  public static void mergesort(int[] data) {
+
+  }
+  /* pseudocode
+  mergesort(data,lo,hi){
+    if more than 1 element{
+      mergesort left side
+      mergesort right side
+      merge
+    }
+  }
+  */
+  public static void mergesort(int[] data, int lo, int hi) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+  
+}
