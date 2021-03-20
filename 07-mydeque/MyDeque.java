2a117578c27e049b25213a2aecefe985dedc0558
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-20 16:44:27 -0400

    wrote addLast

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -46,7 +46,7 @@ public class MyDeque<E>{
     if (element == null) throw new NullPointerException(); //if the specified element is null
     if (size == data.length) resize(); //if the array is filled with elements
     if (size != 0) { //if size is zero, start doesn't need to be changed
-      if (start == 0) { //if start is zero, front moves to the back
+      if (start == 0) { //if start is zero, front moves to the back of array
         start = data.length - 1; //loop around the back
       } else {
         start--; //decrease start to add to left of front
@@ -57,12 +57,30 @@ public class MyDeque<E>{
     size++;
   }
 
+  public void addLast(E element) {
+    if (element == null) throw new NullPointerException(); //if the specified element is null
+    if (size == data.length) resize(); //if the array is filled with elements
+    if (size != 0) { //if size is zero, end doesn't need to be changed
+      if (end == data.length - 1) { //if end is the last index, back moves to the front of array
+        end = 0; //loop around the front
+      } else {
+        end++; //increase end to add to the right of back
+      }
+    }
+    //System.out.println(end); //debugging
+    data[end] = element;
+    size++;
+  }
+
   public static void main(String[] args) {
     MyDeque <Integer> d = new MyDeque<Integer>();
-    System.out.println(d);
     d.addFirst(1);
     System.out.println(d);
     d.addFirst(2);
+    System.out.println(d);
+    d.addLast(3);
+    System.out.println(d);
+    d.addLast(4);
     System.out.println(d);
   }
 
