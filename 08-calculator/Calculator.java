b3777a5d414a4643c25998f444482a93bee492fc
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-22 11:55:58 -0400

    added numbers to the stack

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Calculator {
   /*Evaluate a postfix expression stored in s.
   *Assume valid postfix notation, of ints doubles and operators separated by spaces.
@@ -8,10 +10,27 @@ public class Calculator {
   */
   public static double eval(String s) {
     String[] expression = s.split(" "); //convert your string into tokens (a list of values and operators)
+    ArrayDeque<Double> stack = new ArrayDeque<Double>(); //use a Deque to store your values and act as the stack
+
     for (String token: expression) {
-      System.out.println(token); //test your tokens by printing each one!
+      //System.out.println(token); //test your tokens by printing each one!
+      //instead of printing each one, decide what you should do with them and the stack (operators are never stored)
+      if (token.equals("+")) { //addition
+
+      } else if (token.equals("-")) { //subtraction
+
+      } else if (token.equals("/")) { //division
+
+      } else if (token.equals("*")) { //multiplication
+
+      } else if (token.equals("%")) { //modulo
+
+      } else { //if token is a number and not an operator
+        stack.addFirst(Double.parseDouble(token)); //add number to top of stack
+      }
     }
-    return 0;
+    //System.out.println(stack); //testing tokens
+    return stack.getFirst(); //return solution
   }
 
   public static void main(String[] args) {
