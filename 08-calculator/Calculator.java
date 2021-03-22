40c33e35cce0b7c94673038b7d7dbc07767a3179
Rachel Xiao <rachelxiao907@gmail.com>
2021-03-22 11:38:08 -0400

    converted string into tokens

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -6,7 +6,16 @@ public class Calculator {
   *@throws IllegalArgumentException when there are too many or too few operands.
   *        Use the string parameter of your exception to indicate what happened.
   */
-    public static double eval(String s) {
-      return 0;
+  public static double eval(String s) {
+    String[] expression = s.split(" "); //convert your string into tokens (a list of values and operators)
+    for (String token: expression) {
+      System.out.println(token); //test your tokens by printing each one!
     }
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(eval("11 3 - 4 + 2.5 *"));
+  }
+
 }
