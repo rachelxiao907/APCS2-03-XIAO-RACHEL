fa5e16ea2a1d375781d2deb923773a274b87b48a
Rachel Xiao <rachelxiao907@gmail.com>
2021-04-13 11:54:51 -0400

    frontier class

---
+++ 09-treeburning/Frontier.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Frontier {
+  private ArrayDeque<Integer> frontier;
+  public Frontier() {}
+  public int size() {}
+  public void add(int[] location) {}
+  public int[] remove() {}
+}
