c620c2ee6257fd32d29fb5ec0357150a68470196
Rachel Xiao <rachelxiao907@gmail.com>
2021-04-21 14:43:41 -0400

    blueprint

---
+++ 10-heapsort/MyHeap.java
@@ -0,0 +1,22 @@
+public class MyHeap {
+  /*Swap the element at the provided index downward into the correct position.
+  This will swap with the larger of the child nodes provided thatchild is larger.
+  This stops when a leaf is reached, or neither child is larger.
+  *@param size the number of heap elements in the data array.
+          This is needed to allow a partially full array to be provided.
+  *@precondition the children of data[index] are valid heaps.
+  *@precondition index is between 0 and size-1 inclusive
+  *@precondition size is between 0 and data.length-1 inclusive.
+  */
+  private static void pushDown (int[]data, int size, int index) {
+
+  }
+
+  /*Reorder the provided array to be a valid heap.
+  *@param data is the array to be modified
+  */
+  public static void buildHeap (int[]data) {
+
+  }
+  
+}
